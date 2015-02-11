c916169ee4df4d4142a315e23c8c33deb5a49ad6
Clive Shirley <clive.shirley@mac.com>
2015-02-11 18:02:31 +0100

    Added de-duplication check

--- lib/startup_loan/models/applicant.rb
+++ lib/startup_loan/models/applicant.rb
@@ -1,7 +1,13 @@
 module StartupLoan
   class Applicant < BaseModel
-     def is_duplicate?
-       has_attribute?("DUPLICATE")
-     end
+    def self.has_duplicate?(client, params)
+      self.find_duplicates(client,params).count > 0
+    end
+    def self.find_duplicates(client, params)
+      find(client, params).select { |applicant| applicant.is_duplicate? }
+    end
+    def is_duplicate?
+      has_attribute?("DUPLICATE")
+    end
   end
 end
